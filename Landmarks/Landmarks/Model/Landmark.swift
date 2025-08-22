a6ae7e1546f30bc08a9a32e0558bd8793fbea28c
Fromugh <96973078+Fromugh@users.noreply.github.com>
2025-08-22 16:34:06 +0900

    Tutorials

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -25,7 +25,7 @@ struct Landmark: Hashable, Codable, Identifiable {
         case mountains = "Mountains"
     }
     private var imageName: String
-    var image: Image { // image 변수는 Landmark 구조체의 각 인스턴스에 해당하는 imageName 변수가 저장되면 이에 해당하는 Image 뷰로 리턴함.(imageName에는 저장된 이미지의 파일 이름에 해당하는 값들이 저장되어 있음)
+    var image: Image {
         Image(imageName)
     }
     var featureImage: Image? {
